25a7d2f81bbbc79ee80656535a70348a8d4da386
Accele20 <56901969+Accele20@users.noreply.github.com>
2020-11-30 11:55:49 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,13 @@
+package ex;
+
+public class App extends R{
+	public static void main(String[] args) {
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[0].length; j++) {
+				System.out.print(array2d[i][j]);
+			}
+		}
+	}
+}
