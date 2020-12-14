8367e3b395c4fe761bf8294a81e0dbd9cf2dbbe3
Accele20 <56901969+Accele20@users.noreply.github.com>
2020-12-14 12:04:42 +0800

    Create 121402.奇数求和.App.java

---
+++ 121402.奇数求和.App.java
@@ -0,0 +1,11 @@
+public class jishu {
+    public static void main(String[] args) {
+        long num=0;
+        long j=2147483647;
+        int i=1;
+        if (j%2==1){
+                num=(i+j)*((j-1)/2+1)/2;
+        }
+        System.out.println(num);
+    }
+}
